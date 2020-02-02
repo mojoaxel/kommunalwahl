c41477cd415c910b9739fbf72ce0abda7c915d6c
Alexander Wunschik <alex@wunschik.net>
2020-02-02 16:41:57 +0100

    refactor: add angular and @angular/material

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Kommunalwahl';
+  year = '2020';
+  town = 'Fürth';
+}
